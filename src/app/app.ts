e015c8ffd3f1b21e3d000b633e5a663e0c654b3e
Shovon96 <fakhruddinshovon@gmail.com>
2025-06-16 03:53:25 +0600

    create note successfully

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,30 @@
 import express, { Application, Request, Response } from 'express';
+import { model, Schema } from 'mongoose';
 
 const app: Application = express();
+
+const noteSchema = new Schema({
+    title: String,
+    constent: String
+});
+
+const Note = model("Note", noteSchema);
+
+app.post('/create-note', async (req: Request, res: Response) => {
+    const myNote = new Note({
+        title: 'Mongoose',
+        constent: 'I am learning mongoose'
+    })
+
+    await myNote.save();
+
+    res.status(201).json({
+        success: true,
+        message: 'Note created successfully',
+        note: myNote
+    })
+
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Note Server');
